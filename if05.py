ae737ba3a90ee4fa1166f6805dc5352063785596
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-09 23:47:36 +0500

    nth

--- if05.py
+++ if05.py
@@ -1,9 +1,24 @@
 def main(n):
-    """
+    a=n//10000
+    b=n//1000%10
+    c=n//100%10
+    d=n//10%10
+    e=n%10
+    if a>=b and a>=c and a>=d and a>=e:
+        return a
+    elif b>=a and b>=c and b>=d and b>=e:
+        return b
+    elif c>=a and c>=b and c>=d and c>=e:
+        return c
+    elif d>=a and d>=b and d>=c and d>=e:
+        return d
+    elif e>=a and e>=b and e>=c and e>=d:
+        return e
+print(main(57537))
+"""
     Find the largest digit of the five-digit number.
     Args:
         n: Five-digit number.
     Returns:
         int: return answer.
     """
-    return
