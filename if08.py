e3f3e7bad394a202506db9aa49deeb710dd254b0
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-10 00:07:00 +0500

    nth

--- if08.py
+++ if08.py
@@ -1,5 +1,20 @@
 def main(number):
-    """
+    if number%7==1:
+        return "Monday"
+    elif number%7==2:
+        return "Tuesday"
+    elif number%7==3:
+        return "Wednesday"
+    elif number%7==4:
+        return "Thursday"
+    elif number%7==5:
+        return "Friday"
+    elif number%7==6:
+        return "Saturday"
+    elif number%7==0:
+        return "Sunday"
+print(main(34))
+"""
     Return the days of the week, return the days of the week according to the numbers 1 to 7.
     Use the elif statments.
     1: "Monday"
@@ -14,4 +29,3 @@ def main(number):
     Returns:
         str: return answer.
     """
-    return
