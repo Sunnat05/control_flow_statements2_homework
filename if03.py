2eae5c76bf340df166abc36330c2a8db6eb17805
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-10 00:21:33 +0500

    nth

--- if03.py
+++ if03.py
@@ -1,10 +1,10 @@
 def main(a,b,c):
-    if b<a<c or c<a<b:
-        return "First number"
-    elif a<b<c or c<b<a:
-        return "Second number"
-    elif a<c<b or b<c<a:
-        return "Third number"
+    if b<=a<=c or c<=a<=b:
+        return a
+    elif a<=b<=c or c<=b<=a:
+        return b
+    elif a<=c<=b or b<=c<=a:
+        return c
 print(main(4,8,0))
 """    Determine the number between large and small.
     Args:
