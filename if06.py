2de1b641b77b74663057af163f2828c0b026d294
Sunnat05 <abdurashidovsunnat71@gmail.com>
2023-07-10 10:03:00 +0500

    nth

--- if06.py
+++ if06.py
@@ -5,16 +5,16 @@ def main(n):
      d=n//10%10
      e=n%10
      if a>=b and a>=c and a>=d and a>=e:
-        return a
+        return 5
      elif b>=a and b>=c and b>=d and b>=e:
-        return b
+        return 4
      elif c>=a and c>=b and c>=d and c>=e:
-        return c
+        return 3
      elif d>=a and d>=b and d>=c and d>=e:
-        return d
+        return 2
      elif e>=a and e>=b and e>=c and e>=d:
-        return e
-print(main(57537))
+        return 1
+print(main(58537))
 """
     Find the index of the largest digit of the five-digit number.
     Args:
